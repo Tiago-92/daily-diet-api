4010657e533b03715e6d600b6b34486797240783
Tiago-92 <tiagoamaralcbei@hotmail.com>
2024-02-03 14:52:34 -0300

    feat: criação das tabelas do banco

---
+++ migrations/20240203135103_meals.ts
@@ -0,0 +1,17 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('meals', (table) => {
+    table.uuid('id')
+    table.string('user_id').references('id').inTable('users')
+    table.text('name')
+    table.text('description')
+    table.boolean('isDiet')
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+    table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('meals')
+}
